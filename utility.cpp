70f328fb6096ea75462e71e081540417e57bad9f
Christopher Andrade <christopherandrade@cs.ucsb.edu>
2022-02-25 10:40:21 -0800

    done

--- utility.cpp
+++ utility.cpp
@@ -1,134 +1,134 @@
-#include <iostream>
-#include <map>
-#include "utility.h"
-using namespace std;
+// #include <iostream>
+// #include <map>
+// #include "utility.h"
+// using namespace std;
 
-Cards::Cards() : suit('z'), val("0") {}
-Cards::~Cards() {}
+// Cards::Cards() : suit('z'), val("0") {}
+// Cards::~Cards() {}
 
-Cards::Cards(char s, string v)
-{
-    suit = s;
-    val = v;
-}
+// Cards::Cards(char s, string v)
+// {
+//     suit = s;
+//     val = v;
+// }
 
-void Cards::setCard(char &suit, string &val)
-{
-    this->suit = suit;
-    this->val = val;
-}
-char Cards::getSuit() const
-{
-    return suit;
-}
+// void Cards::setCard(char &suit, string &val)
+// {
+//     this->suit = suit;
+//     this->val = val;
+// }
+// char Cards::getSuit() const
+// {
+//     return suit;
+// }
 
-string Cards::getVal() const
-{
-    return val;
-}
+// string Cards::getVal() const
+// {
+//     return val;
+// }
 
-bool operator<(Cards const &c1, Cards const &c2)
-{
+// bool operator<(Cards const &c1, Cards const &c2)
+// {
 
-    if (c1.suit == c2.suit && c1.val == c2.val)
-        return false;
+//     if (c1.suit == c2.suit && c1.val == c2.val)
+//         return false;
 
-    int suit1;
-    switch (c1.suit)
-    {
-    case 'c':
-        suit1 = 1;
-        break;
+//     int suit1;
+//     switch (c1.suit)
+//     {
+//     case 'c':
+//         suit1 = 1;
+//         break;
 
-    case 'd':
-        suit1 = 2;
-        break;
+//     case 'd':
+//         suit1 = 2;
+//         break;
 
-    case 's':
-        suit1 = 3;
-        break;
+//     case 's':
+//         suit1 = 3;
+//         break;
 
-    case 'h':
-        suit1 = 4;
-        break;
-    }
+//     case 'h':
+//         suit1 = 4;
+//         break;
+//     }
 
-    int suit2;
-    switch (c2.suit)
-    {
-    case 'c':
-        suit2 = 1;
-        break;
+//     int suit2;
+//     switch (c2.suit)
+//     {
+//     case 'c':
+//         suit2 = 1;
+//         break;
 
-    case 'd':
-        suit2 = 2;
-        break;
+//     case 'd':
+//         suit2 = 2;
+//         break;
 
-    case 's':
-        suit2 = 3;
-        break;
+//     case 's':
+//         suit2 = 3;
+//         break;
 
-    case 'h':
-        suit2 = 4;
-        break;
-    }
+//     case 'h':
+//         suit2 = 4;
+//         break;
+//     }
 
-    map<string, int> cardFace;
-    cardFace["a"] = 1;
-    cardFace["2"] = 2;
-    cardFace["3"] = 3;
-    cardFace["4"] = 4;
-    cardFace["5"] = 5;
-    cardFace["6"] = 6;
-    cardFace["7"] = 7;
-    cardFace["8"] = 8;
-    cardFace["9"] = 9;
-    cardFace["10"] = 10;
-    cardFace["j"] = 11;
-    cardFace["q"] = 12;
-    cardFace["k"] = 13;
+//     map<string, int> cardFace;
+//     cardFace["a"] = 1;
+//     cardFace["2"] = 2;
+//     cardFace["3"] = 3;
+//     cardFace["4"] = 4;
+//     cardFace["5"] = 5;
+//     cardFace["6"] = 6;
+//     cardFace["7"] = 7;
+//     cardFace["8"] = 8;
+//     cardFace["9"] = 9;
+//     cardFace["10"] = 10;
+//     cardFace["j"] = 11;
+//     cardFace["q"] = 12;
+//     cardFace["k"] = 13;
 
-    auto val1 = cardFace.find(c1.val);
-    auto val2 = cardFace.find(c2.val);
+//     auto val1 = cardFace.find(c1.val);
+//     auto val2 = cardFace.find(c2.val);
 
-    if (suit1 < suit2)
-    {
-        return true;
-    }
-    else if (suit1 > suit2)
-    {
-        return false;
-    }
-    else // COMPARING THE VALUES
-    {
-        if (val1->second < val2->second)
-        {
-            return true;
-        }
-        else
-            return false;
-    }
-}
+//     if (suit1 < suit2)
+//     {
+//         return true;
+//     }
+//     else if (suit1 > suit2)
+//     {
+//         return false;
+//     }
+//     else // COMPARING THE VALUES
+//     {
+//         if (val1->second < val2->second)
+//         {
+//             return true;
+//         }
+//         else
+//             return false;
+//     }
+// }
 
-bool operator>(Cards const &c1, Cards const &c2)
-{
-    if (c1.suit == c2.suit && c1.val == c2.val)
-        return false;
-    if (c1 < c2)
-        return false;
-    else
-        return true;
-}
+// bool operator>(Cards const &c1, Cards const &c2)
+// {
+//     if (c1.suit == c2.suit && c1.val == c2.val)
+//         return false;
+//     if (c1 < c2)
+//         return false;
+//     else
+//         return true;
+// }
 
-bool operator==(Cards const &c1, Cards const &c2)
-{
-    if (c1.suit == c2.suit && c1.val == c2.val)
-        return true;
-    else
-        return false;
-}
+// bool operator==(Cards const &c1, Cards const &c2)
+// {
+//     if (c1.suit == c2.suit && c1.val == c2.val)
+//         return true;
+//     else
+//         return false;
+// }
 
-void Cards::printCard() const
-{
-    cout << this->suit << " " << this->val << endl;
-}
+// void Cards::printCard() const
+// {
+//     cout << this->suit << " " << this->val << endl;
+// }
--- utility.h
+++ utility.h
@@ -1,29 +1,29 @@
-#ifndef UTILITY_H
-#define UTILITY_H
+// #ifndef UTILITY_H
+// #define UTILITY_H
 
-#include <iostream>
-#include <map>
-using namespace std;
+// #include <iostream>
+// #include <map>
+// using namespace std;
 
-class Cards
-{
-public:
-    Cards();
-    ~Cards();
-    Cards(char s, string v);
-    void setCard(char &suit, string &val);
-    char getSuit() const;
-    string getVal() const;
-    void printCard() const;
+// class Cards
+// {
+// public:
+//     Cards();
+//     ~Cards();
+//     Cards(char s, string v);
+//     void setCard(char &suit, string &val);
+//     char getSuit() const;
+//     string getVal() const;
+//     void printCard() const;
 
-    friend bool operator<(Cards const &c1, Cards const &c2);
-    friend bool operator>(Cards const &c1, Cards const &c2);
-    friend bool operator==(Cards const &c1, Cards const &c2);
+//     friend bool operator<(Cards const &c1, Cards const &c2);
+//     friend bool operator>(Cards const &c1, Cards const &c2);
+//     friend bool operator==(Cards const &c1, Cards const &c2);
 
-private:
-    char suit;
-    string val;
-    map<string, int> cardFace;
-};
+// private:
+//     char suit;
+//     string val;
+//     map<string, int> cardFace;
+// };
 
-#endif
+// #endif
